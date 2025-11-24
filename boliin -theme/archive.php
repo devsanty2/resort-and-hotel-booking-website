8a3b3537a85e59c60f92abc71bf82f1489c1d537
Santy akther <infosantya@gmail.com>
2025-11-24 22:16:19 +0600

    Add files via upload

---
+++ boliin -theme/archive.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *
+ * @author     Gaviasthemes     
+ * @copyright  Copyright (C) 2023 Gaviasthemes. All Rights Reserved.
+ * @license    GNU/GPL v2 or later http://www.gnu.org/licenses/gpl-2.0.html
+ * 
+ */
+	get_header(); 
+	
+?>
+
+<section id="wp-main-content" class="clearfix main-page">
+	<?php do_action('boliin_before_page_content'); ?>
+	<div class="main-page-content">
+		<div class="content-page">      
+			<div id="wp-content" class="wp-content clearfix">
+				<?php 
+					if(class_exists('GVA_Layout_Frontend')){
+						do_action('boliin/layouts/archive/post');
+					}else{
+						get_template_part('templates/blog/archive');
+					}
+				?>
+			</div>    
+		</div>      
+	</div>   
+	<?php do_action('boliin_after_page_content'); ?>
+</section>
+
+<?php get_footer(); ?>
+
